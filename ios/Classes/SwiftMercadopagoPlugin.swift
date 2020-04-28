c715d1955e1a9dd473af069d41547693e01d566d
jagu <jagu@clarika.com.ar>
2020-04-28 18:52:10 -0300

    WIP iOS - pending get the current UINavigationController to start the checkout process

--- ios/Classes/SwiftMercadopagoPlugin.swift
+++ ios/Classes/SwiftMercadopagoPlugin.swift
@@ -1,14 +1,67 @@
 import Flutter
 import UIKit
+import MercadoPagoSDK
 
-public class SwiftMercadopagoPlugin: NSObject, FlutterPlugin {
+public class SwiftMercadopagoPlugin: NSObject, FlutterPlugin, PXLifeCycleProtocol {
+       
+  var flutterResult: FlutterResult? = nil
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "mercadopago", binaryMessenger: registrar.messenger())
+    let messenger = registrar.messenger()
+    let channel = FlutterMethodChannel(name: "mercadopago", binaryMessenger: messenger)
     let instance = SwiftMercadopagoPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    
+    if(call.method == "startPayment"){
+        startPayment(result, call)
+    }
   }
+ 
+    func startPayment(_ result: @escaping FlutterResult, _ call: FlutterMethodCall) {
+        self.flutterResult = result
+        let arguments = call.arguments as! NSDictionary
+        let publicKey = arguments["publicKey"] as! String
+        let checkoutPreferenceId = arguments["checkoutPreferenceId"] as! String
+        
+        let navigationController = findNavigationController()
+        openMercadoPagoCheckout(publicKey, checkoutPreferenceId, navigationController)
+            
+        result("execute" + publicKey + checkoutPreferenceId);
+    }
+
+     public func cancelCheckout() -> (() -> Void)? {
+         self.flutterResult!("cancelCheckout")
+         return nil
+     }
+     
+     public func finishCheckout() -> ((PXResult?) -> Void)? {
+         self.flutterResult!("finishCheckout")
+         return nil
+     }
+    
+    fileprivate func findNavigationController() -> UINavigationController {
+        /*let delegate = UIApplication.shared.delegate
+        let delegateWindow = delegate!.window!
+        let rootController = (delegateWindow!.rootViewController)!
+        return rootController.navigationController!*/
+        let windows = UIApplication.shared.windows.filter( { (w) -> Bool in
+            (w.rootViewController != nil && w.rootViewController?.navigationController != nil)
+            })
+        
+        return windows.last!.rootViewController!.navigationController!
+    }
+    
+    fileprivate func openMercadoPagoCheckout(_ publicKey: String, _ checkoutPreferenceId: String, _ navigationController: UINavigationController) {
+          MercadoPagoCheckout.init(
+              builder: MercadoPagoCheckoutBuilder
+                  .init(
+                      publicKey: publicKey,
+                      preferenceId: checkoutPreferenceId))
+              .start(
+                  navigationController: navigationController,
+                  lifeCycleProtocol: self)
+      }
 }
--- ios/mercadopago.podspec
+++ ios/mercadopago.podspec
@@ -15,10 +15,10 @@ mercado pago
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.dependency 'MercadoPagoSDK'
-  s.platform = :ios, '8.0'
+  s.dependency 'MercadoPagoSDK', '~>4.32.4'
+  s.static_framework = true
+  s.platform = :ios, '10.0'
 
   # Flutter.framework does not contain a i386 slice. Only x86_64 simulators are supported.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'VALID_ARCHS[sdk=iphonesimulator*]' => 'x86_64' }
-  s.swift_version = '5.0'
 end
