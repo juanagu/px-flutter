d84a7a9a1883a65b83a9558a4ddada3be513e9b3
jagu <jagu@clarika.com.ar>
2020-05-05 10:38:07 -0300

    fix iOS navigation - WIP result

--- ios/Classes/SwiftMercadopagoPlugin.swift
+++ ios/Classes/SwiftMercadopagoPlugin.swift
@@ -4,12 +4,19 @@ import MercadoPagoSDK
 
 public class SwiftMercadopagoPlugin: NSObject, FlutterPlugin, PXLifeCycleProtocol {
        
-  var flutterResult: FlutterResult? = nil
+    var flutterResult: FlutterResult? = nil
+    var delegateWindow : UIWindow
+    
+    init(delegateWindow: UIWindow){
+        self.delegateWindow = delegateWindow
+    }
     
   public static func register(with registrar: FlutterPluginRegistrar) {
     let messenger = registrar.messenger()
     let channel = FlutterMethodChannel(name: "mercadopago", binaryMessenger: messenger)
-    let instance = SwiftMercadopagoPlugin()
+
+    let delegateWindow = UIApplication.shared.delegate!.window!!
+    let instance = SwiftMercadopagoPlugin(delegateWindow: delegateWindow)
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
@@ -25,11 +32,11 @@ public class SwiftMercadopagoPlugin: NSObject, FlutterPlugin, PXLifeCycleProtoco
         let arguments = call.arguments as! NSDictionary
         let publicKey = arguments["publicKey"] as! String
         let checkoutPreferenceId = arguments["checkoutPreferenceId"] as! String
-        
-        let navigationController = findNavigationController()
-        openMercadoPagoCheckout(publicKey, checkoutPreferenceId, navigationController)
-            
-        result("execute" + publicKey + checkoutPreferenceId);
+        if(UIApplication.shared.keyWindow!.rootViewController! is UINavigationController){
+            openMercadoPagoCheckout(publicKey, checkoutPreferenceId, UIApplication.shared.keyWindow!.rootViewController! as! UINavigationController)
+        }else{
+            result("Check the documentation and implement the UINavigationController on Runner.AppDelegate");
+        }
     }
 
      public func cancelCheckout() -> (() -> Void)? {
@@ -41,18 +48,6 @@ public class SwiftMercadopagoPlugin: NSObject, FlutterPlugin, PXLifeCycleProtoco
          self.flutterResult!("finishCheckout")
          return nil
      }
-    
-    fileprivate func findNavigationController() -> UINavigationController {
-        /*let delegate = UIApplication.shared.delegate
-        let delegateWindow = delegate!.window!
-        let rootController = (delegateWindow!.rootViewController)!
-        return rootController.navigationController!*/
-        let windows = UIApplication.shared.windows.filter( { (w) -> Bool in
-            (w.rootViewController != nil && w.rootViewController?.navigationController != nil)
-            })
-        
-        return windows.last!.rootViewController!.navigationController!
-    }
     
     fileprivate func openMercadoPagoCheckout(_ publicKey: String, _ checkoutPreferenceId: String, _ navigationController: UINavigationController) {
           MercadoPagoCheckout.init(
